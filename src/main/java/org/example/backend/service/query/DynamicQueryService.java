76e9d0d1a3c5c2747be0bcdacca4a1a965d54b5d
PabloLec <pablo.lecolinet@gmail.com>
2024-05-04 11:47:30 +0200

    Refactor QueryDsl class loading

--- src/main/java/org/example/backend/service/query/DynamicQueryService.java
+++ src/main/java/org/example/backend/service/query/DynamicQueryService.java
@@ -5,27 +5,26 @@ import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.backend.service.query.querydsl.QueryDslClassMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 import static org.example.backend.service.query.ExpressionBuilder.buildExpression;
 import static org.example.backend.service.query.ExpressionBuilder.buildSubQueryExpression;
-import static org.example.backend.service.query.QueryDslUtils.getEntityPathBase;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class DynamicQueryService {
 
     private final EntityManager entityManager;
-
-    public DynamicQueryService(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    private final QueryDslClassMapper queryDslClassMapper;
 
     public JPAQuery<?> buildDynamicQuery(List<SearchCriterion> criteria, String entityPathString) {
-        EntityPathBase<?> entityPathBase = getEntityPathBase(entityPathString);
+        EntityPathBase<?> entityPathBase = queryDslClassMapper.getEntityPathBase(entityPathString);
         JPAQuery<?> query = new JPAQuery<>(entityManager).from(entityPathBase);
 
         for (SearchCriterion criterion : criteria) {
@@ -40,7 +39,7 @@ public class DynamicQueryService {
 
     private BooleanExpression handleSubQuery(SearchCriterion criterion, EntityPathBase<?> rootEntityPath, String rootEntityPathString) {
         String relatedEntityName = criterion.getField();
-        EntityPathBase<?> relatedEntityPath = getEntityPathBase(relatedEntityName);
+        EntityPathBase<?> relatedEntityPath = queryDslClassMapper.getEntityPathBase(relatedEntityName);
         PathBuilder<?> relatedEntityBuilder = new PathBuilder<>(relatedEntityPath.getType(), relatedEntityPath.getMetadata().getName());
         PathBuilder<?> rootEntityBuilder = new PathBuilder<>(rootEntityPath.getType(), rootEntityPath.getMetadata().getName());
         BooleanExpression fieldCondition = buildExpression(relatedEntityBuilder, criterion.getSubCriterion());
---
+++ src/main/java/org/example/backend/service/query/querydsl/QueryDslClassMapper.java
@@ -0,0 +1,36 @@
+package org.example.backend.service.query.querydsl;
+
+import com.querydsl.core.types.dsl.EntityPathBase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Component
+public class QueryDslClassMapper {
+    private final Map<String, EntityPathBase<?>> entityMappings;
+
+    @Autowired
+    public QueryDslClassMapper(Map<String, EntityPathBase<?>> entityMappings) {
+        if (entityMappings == null || entityMappings.isEmpty()) {
+            throw new IllegalArgumentException("Entity mappings cannot be null or empty");
+        }
+        this.entityMappings = Collections.unmodifiableMap(entityMappings);
+    }
+
+    public EntityPathBase<?> getEntityPathBase(String key) {
+        if (key == null || key.isEmpty()) {
+            throw new IllegalArgumentException("Key is null or empty");
+        }
+        EntityPathBase<?> entityPath = entityMappings.get(key);
+        if (entityPath == null) {
+            throw new IllegalStateException("No matching EntityPathBase found for '" + key + "'");
+        }
+        return entityPath;
+    }
+
+    public Map<String, EntityPathBase<?>> getMappings() {
+        return entityMappings;
+    }
+}
