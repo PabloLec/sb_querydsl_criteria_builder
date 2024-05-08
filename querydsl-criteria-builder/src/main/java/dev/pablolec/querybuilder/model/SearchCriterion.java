409d459ec8750ed257387596cc4ddda599ee55d5
PabloLec <pablo.lecolinet@gmail.com>
2024-05-08 17:43:03 +0200

    Add NoArgsConstructor to SearchCriterion

--- querydsl-criteria-builder/src/main/java/dev/pablolec/querybuilder/model/SearchCriterion.java
+++ querydsl-criteria-builder/src/main/java/dev/pablolec/querybuilder/model/SearchCriterion.java
@@ -3,9 +3,11 @@ package dev.pablolec.querybuilder.model;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SearchCriterion {
     private String field;
     private String op;
